6c3290e54c8bd199ea1c9118fbdba3eaa55d7485
Rosario-Iaccarino <iaccarinorosario@gmail.com>
2022-09-09 17:08:38 +0200

    General update

---
+++ TargetFinder/TargetFinder.m
@@ -0,0 +1,34 @@
+function [result,Tcoord,plots] = TargetFinder(photoA,photoB)
+
+
+% Set variables -----------------------------------------------------------
+% set preprocessing variables
+set_prepro.thres = 0.4;    % threshold filter
+set_prepro.med = 3;        % dimension of the median filter
+% set the dimension of the filter
+conv_len = size(photoA,1);
+%--------------------------------------------------------------------------
+
+% pre processing
+[photoA,photoB] = prepro(photoA,photoB,set_prepro);
+
+
+% correlation
+[corr_mat] = correlate_photos(photoA,photoB,conv_len);
+
+
+% find relative coordinates between the two images
+[x0,y0] = find_imagecoord(corr_mat, conv_len);
+
+% compute target coordinate in the two photos
+[Tcoord,result,diff_cropped] = find_Tcoord(x0,y0,photoA,photoB);
+
+plots.corr_mat = corr_mat;
+plots.diff_cropped = diff_cropped;
+
+
+
+
+
+end
+
