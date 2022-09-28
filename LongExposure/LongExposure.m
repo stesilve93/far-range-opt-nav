19dcfaa08757223af6a930d42ea58b6752f03f84
Rosario-Iaccarino <iaccarinorosario@gmail.com>
2022-09-28 19:00:11 +0200

    HoldingPoind simulation

--- LongExposure/LongExposure.m
+++ LongExposure/LongExposure.m
@@ -8,24 +8,27 @@ sky_raw = im2double(sky_raw);                   % resize between [0,1]
 figure
 imshow(sky_raw)
 
+% sky_raw = sky_raw(100:200,200:end);
+
 % preprocessing
 set_prepro.med = 3;
 set_prepro.thres = 0.3;
 [sky] = prepro(sky_raw,set_prepro);
+% sky = imbinarize(sky_raw);
 % sky = edge(sky,'canny');
 % sky = bwmorph(sky,'thin');
 
-figure
-imshow(sky)
+% figure
+% imshow(sky)
 
 % Hough Transform Agorithm
 [H, T, R] = hough(sky);
 P = houghpeaks(H,1);
 L = houghlines(sky,T,R,P,'MinLength',5);
 
-figure
-imshow(H); hold on
-plot(P(1,2),P(1,1),'r*'); hold on
+% figure
+% imshow(H); hold on
+% plot(P(1,2),P(1,1),'r*'); hold on
 
 figure
 imshow(sky_raw); hold on
